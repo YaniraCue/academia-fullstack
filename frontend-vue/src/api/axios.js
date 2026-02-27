a27f362f6b56d78861e13d5db86ee7d170b0c62f
YaniraCue <yanirabdullah@gmail.com>
2026-02-27 20:41:37 +0000

    Cambiar baseURL a la API de Render

--- frontend-vue/src/api/axios.js
+++ frontend-vue/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // La URL de tu Laravel en WSL
+    baseURL: 'https://academia-backend-7j5n.onrender.com/api', // La URL de tu Laravel en WSL
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
